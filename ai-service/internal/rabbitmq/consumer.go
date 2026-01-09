4e801f51daf9882964e67c2045e2d072afd98412
Azizbekweb098 <webcoderazizbek@gmail.com>
2026-01-09 19:50:44 +0500

    arena

--- ai-service/internal/rabbitmq/consumer.go
+++ ai-service/internal/rabbitmq/consumer.go
@@ -1,9 +1,6 @@
 package rabbitmq
 
 import (
-	"fmt"
-	"log"
-
 	"ai-service/internal/service"
 	"ai-service/internal/workerpool"
 
@@ -16,26 +13,22 @@ func StartConsumer(ch *amqp.Channel, testService *service.TestService) {
 	msgs, err := ch.Consume(
 		queueName,
 		"",
-		true, 
-		false, 
+		true,
 		false,
-		false, 
+		false,
+		false,
 		nil,
 	)
 	if err != nil {
-		log.Fatalf("Queue’dan xabar ololmadi: %v", err)
+		return
 	}
 
-	fmt.Println("🐇 RabbitMQ consumer ishga tushdi...")
-
-	
 	pool := workerpool.New(5, 20, testService)
 	pool.Start()
-
 
 	for msg := range msgs {
 		pool.Submit(msg.Body)
 	}
 
-	select {} 
+	select {}
 }
